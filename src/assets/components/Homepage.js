90aaa3f71fc9259a2be8d12b9c6cd932929038c8
Natalia Takahara <takaharanatalia@gmail.com>
2023-04-26 23:32:07 +0200

    changed homepage and index

--- src/assets/components/Homepage.js
+++ src/assets/components/Homepage.js
@@ -1,8 +1,44 @@
 function Homepage() {
     return (
-        <div>
-            <h1>Homepage</h1>
-        </div>
+        <main>
+            <section>
+            <div class="promotion">
+                <h2>Get 20% off this friday!</h2>
+            </div>
+        </section>
+            <article>
+                <h2>Our Seasonal Menu</h2>
+            
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </article>
+            <article>
+                <h2>Opening Hours</h2>
+                <table id="open">
+                    <tr>
+                        <th>Day </th>
+                        <th>Opening hours</th>
+                    </tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr>
+                        <td>Tuesday </td>
+                        <td>17:00 - 22:00</td>
+                    </tr>
+                    <tr>
+                        <td>Friday </td>
+                        <td>17:00 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <td>Saturday </td>
+                        <td>11:30 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday </td>
+                        <td>11:30 - 21:00</td>
+                    </tr>
+                </table>
+            </article>
+        </main>
     );
 
 }
